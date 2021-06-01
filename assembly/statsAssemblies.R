0b47a9db461526a5cec8d6ecd6f73248ec5eeb4f
Hannelore Longin <61316639+hannelorelongin@users.noreply.github.com>
2021-06-01 14:37:28 +0200

    Update statsAssemblies.R

--- assembly/statsAssemblies.R
+++ assembly/statsAssemblies.R
@@ -1,8 +1,8 @@
-##Comparison of different assemblers 
+##Statistical comparison of different assemblers 
 
-##Step 0: clearing the environment, reading in the data
+##clearing the environment, reading in the data
 rm(list=ls())
-data=read.table("contN50Size_ok.txt", header=TRUE)
+data=read.table("contLengthN50.txt", header=TRUE)
 
 ##checking normality
 shapiro.test(data$contigs) ##significantly different from normal distribution
@@ -15,15 +15,15 @@ kruskal.test(length ~ assembler, data = data) ##not significant
 kruskal.test(N50 ~ assembler, data = data) ##significant
 
 ##find significant pairs
-pairwise.wilcox.test(data$contigs, data$assembler, p.adjust.method = "BH") #all sign except flye - uni
-pairwise.wilcox.test(data$N50, data$assembler, p.adjust.method = "BH") #all sign except flye - uni
+pairwise.wilcox.test(data$contigs, data$assembler, p.adjust.method = "holm") ##all significant except flye - unicycler
+pairwise.wilcox.test(data$N50, data$assembler, p.adjust.method = "holm") ##all significant except flye - unicycler
 
 ##plotting
 library(ggplot2)
 library(ggpubr)
 library(scales)
 
-my_comparisons <- list( c("flye", "spades"), c("flye", "unicycler"), c("spades", "unicycler") )
+my_comparisons = list(c("flye", "spades"), c("flye", "unicycler"), c("spades", "unicycler"))
  
 plotContigs = ggplot(data, aes(x=assembler, y=contigs, fill=assembler)) +
   geom_boxplot(outlier.shape = NA) + geom_jitter(shape=16, position=position_jitter(0.2)) + 
@@ -31,9 +31,10 @@ plotContigs = ggplot(data, aes(x=assembler, y=contigs, fill=assembler)) +
   scale_y_continuous(labels = scientific)
 plotLength = ggplot(data[data$length < 7306678,], aes(x=assembler, y=length, fill=assembler)) +
   geom_boxplot(outlier.shape = NA) + geom_jitter(shape=16, position=position_jitter(0.2)) + 
-  labs(y="Genome size") + stat_compare_means(label.x.npc = "center") ##+ theme_minimal()
+  labs(y="Genome size") + stat_compare_means(label.x.npc = "center") 
 plotN50 = ggplot(data, aes(x=assembler, y=N50, fill=assembler)) +
-  geom_boxplot(outlier.shape = NA) + geom_jitter(shape=16, position=position_jitter(0.2))  + stat_compare_means(comparisons = my_comparisons) +
+  geom_boxplot(outlier.shape = NA) + geom_jitter(shape=16, position=position_jitter(0.2)) + 
+  stat_compare_means(comparisons = my_comparisons) +
   scale_y_continuous(labels = scientific)
 plotContigs
 plotLength 
